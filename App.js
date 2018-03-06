47b7e63f998b2fd7b3e86bc4fc068dbda81ce421
SecretCastle <chenfangzhong@hotmail.com>
2018-03-06 14:20:14 +0800

    优化问题

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import _sdk from './sdk/sdk';
 
 export default class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    power: '0'
   }
 
   componentDidMount() {
     _sdk.DataBridge.bindPushData((data) => {
       this.setState({
-        data: data.data
+        data: data
       })
     })
+  }
+
+  sendMsg = () => {
+    _sdk.DataBridge.setDeviceStatus({
+      "Switch": this.state.power === '0' ? '1' : '0'
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
         <Text>{JSON.stringify(this.state.data)}</Text>
+        <Button 
+          title="我是一个开关"
+          onPress={this.sendMsg}
+        />
       </View>
     );
   }
--- sdk/sdk.js
+++ sdk/sdk.js
@@ -1,4 +1,4 @@
-const HOST = "192.168.19.71";
+const HOST = "192.168.2.139";
 const PORT = "9901";
 
 const client = new WebSocket(`ws://${HOST}:${PORT}`);
@@ -9,23 +9,96 @@ client.onopen = function() {
 }
 
 /**
+ * 工具类方法
+ */
+const Tools = {
+    paramTransfor(type, data) {
+        /**
+         * 下发数据格式转换函数
+         * 
+         * 上报的结构
+         * {
+         *      "dp_key_array": ["WorkMode", "Power"],
+         *      "dp_value": {
+         *          "WorkMode": "1",
+         *          "Power": "1"   
+         *      }
+         * }
+         * 
+         * 下发的结构
+         * 
+         * {
+         *      "WorkMode": "1",
+         *      "Power": "1"
+         * }
+         */
+        
+         // 接收到的上报数据
+        const innerReceviedFactory = (data) => {
+            // 这里只接受dp_value
+            if (data.dp_value) {
+                return data.dp_value;
+            }
+            return {};
+        }
+
+        // 下发的数据
+        const innerSendFactory = (data) => {
+            const keys = this._keys(data);
+            // const sendData = {
+            //     'dp_key_array': keys,
+            //     'dp_value': data
+            // }
+            return [data];
+        }
+
+        // 判断是上报还是下发
+        
+        if (!type) {
+            throw new Error('please select type');
+        }
+        
+        if (type === 'received') {
+            return innerReceviedFactory(data);
+        } else if (type === 'send') {
+            return innerSendFactory(data);
+        }
+    },
+    _keys(obj) {
+        const hasProperty = Object.prototype.hasOwnProperty;
+        const keys = [];
+        if (toString.call(obj) !== '[object Object]') {
+            return obj;
+        }
+        for (let item in obj) {
+            if (hasProperty.call(obj, item)) {
+                keys.push(item);
+            }
+        }
+        return keys;
+    }
+}
+
+/**
  * SDK 遵循ali接口定义规范
  */
 
 const RNSDK = {
     DataBridge: {
-        isOnline() {
-
-        },
         bindPushData(callback) {
             client.onmessage = function(data) {
+                const receivedData = JSON.parse(data.data).data.data;
                 if (data) {
-                    callback(data);
+                    const transData = Tools.paramTransfor('received', receivedData);
+                    callback(transData);
                 }
             }
         },
-        setDeviceStatus(params) {
-
+        setDeviceStatus(data) {
+            if (data) {
+                const sendData = Tools.paramTransfor('send', data);
+                client.send(JSON.stringify(sendData));
+            }
         }
     }
 }
